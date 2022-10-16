a0dc55acb8fbebda75c5073af91b81323a4e0828
Anilevmoon <Anilevmoon@users.noreply.github.com>
2022-10-16 20:44:09 +0300

    alpha1.2

--- TAsyncDownload.h
+++ TAsyncDownload.h
@@ -3,16 +3,43 @@
 #include <urlmon.h>
 #pragma comment (lib, "urlmon.lib")
 #include <fstream>
-#include <urlmon.h>
 #include <direct.h>
 #include <stdio.h>
 #include <string.h>
 #include <assert.h>
 #include <thread>  
+#include <wininet.h>
+#pragma comment(lib,"wininet")
 
 double c = 0.0;
 double pointsByInrementPer500mls = 0.0;
 auto IndicatUpdating = false;
+
+
+auto FirstExePath = TEXT("C:\\Igrushka\\WindowsNoEditor.part1.exe");
+LPCTSTR Path1exe = FirstExePath;
+auto FirstExeURL = TEXT("http://185.233.36.66:7777/nesty_test/Game/WindowsNoEditor.part1.exe");
+LPCTSTR Path1URL = FirstExeURL;
+
+auto SecondExePath = TEXT("C:\\Igrushka\\WindowsNoEditor.part2.exe");
+LPCTSTR Path2exe = SecondExePath;
+auto SecondExeURL = TEXT("http://185.233.36.66:7777/nesty_test/Game/WindowsNoEditor.part2.exe");
+LPCTSTR Path2URL = SecondExeURL;
+
+auto ThirdExePath = TEXT("C:\\Igrushka\\WindowsNoEditor.part3.exe");
+LPCTSTR Path3exe = ThirdExePath;
+auto ThirdExeURL = TEXT("http://185.233.36.66:7777/nesty_test/Game/WindowsNoEditor.part3.exe");
+LPCTSTR Path3URL = ThirdExeURL;
+
+auto FouthExePath = TEXT("C:\\Igrushka\\WindowsNoEditor.part4.exe");
+LPCTSTR Path4exe = FouthExePath;
+auto FouthExeURL = TEXT("http://185.233.36.66:7777/nesty_test/Game/WindowsNoEditor.part4.exe");
+LPCTSTR Path4URL = FouthExeURL;
+
+auto VERtxtPath = TEXT("C:\\Igrushka\\VERSION.txt");
+LPCTSTR PathVersionTXT = VERtxtPath;
+auto VERtxtURL = TEXT("http://185.233.36.66:7777/nesty_test/Game/version.txt");
+LPCTSTR URLversionTXT = VERtxtURL ;
 
 bool CheckFile() {
 	auto file = std::ifstream("C:\\Igrushka\\MatVkanave.txt");
@@ -23,6 +50,49 @@ bool CheckFile() {
 	}
 }
 
+void V2YskolaichDownload(LPCTSTR Path, LPCTSTR URL) {
+	auto th = std::thread{ [&] {
+	HINTERNET hInternetSession;
+	HINTERNET hURL;
+
+	BOOL bResult;
+	DWORD dwBytesRead = 1;
+
+
+	hInternetSession = InternetOpen(L"", INTERNET_OPEN_TYPE_PRECONFIG, NULL, NULL, 0);
+	hURL = InternetOpenUrl(hInternetSession, URL, NULL, 0, 0, 0);
+
+	char buf[1024];
+	DWORD dwTemp;
+	HANDLE hFile = CreateFile(Path, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	for (; dwBytesRead > 0;)
+	{
+		InternetReadFile(hURL, buf, (DWORD)sizeof(buf), &dwBytesRead);
+		WriteFile(hFile, buf, dwBytesRead, &dwTemp, NULL);
+	}
+
+	InternetCloseHandle(hURL);
+	InternetCloseHandle(hInternetSession);
+
+	CloseHandle(hFile);
+			}
+	};
+	th.detach();
+}
+
+void ATHYNCV2YskolaichDownload() {
+	auto th = std::thread{ [&] {
+
+	V2YskolaichDownload(Path1exe, Path1URL);
+	V2YskolaichDownload(Path2exe, Path2URL);
+	V2YskolaichDownload(Path3exe, Path3URL);
+	V2YskolaichDownload(Path4exe, Path4URL);
+	V2YskolaichDownload(PathVersionTXT, URLversionTXT);
+		}
+	};
+	th.detach();
+}
 
 void AsyncDownloader() {
 	auto th = std::thread{ [] {
